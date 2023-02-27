311561bf01dfa9bfeec30de088b82a5361689a54
amuljoshi <83940325+amuljoshi@users.noreply.github.com>
2023-02-27 15:15:18 +0545

    Commit 20230227

--- Neural Network from Scratch Pt 3.py
+++ Neural Network from Scratch Pt 3.py
@@ -17,6 +17,7 @@ print(output)
 
 '''
 
+'''
 inputs =  [1, 2, 3, 2.5]
 
 weights = [[0.2, 0.8, -0.5, 1.0],
@@ -34,3 +35,26 @@ for neuron_weights, neuron_bias in zip(weights, biases):
     layer_outputs.append(neuron_output)
 
 print(layer_outputs)
+
+'''
+
+'''
+
+inputs =  [1, 2, 3, 2.5]
+
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+some_value = -0.5
+weight = 0.7
+bias = 0.7
+
+print(some_value*weight)
+print(some_value+bias)
+
+'''
+
+
