4916602c0846ee5f2cdc5af08eeb9629e7a90dec
amuljoshi <83940325+amuljoshi@users.noreply.github.com>
2023-03-01 14:17:14 +0545

    Commit

--- Neural Network from Scratch Pt 5.py
+++ Neural Network from Scratch Pt 5.py
@@ -42,6 +42,7 @@ print(output)
 
 import numpy as np
 import nnfs
+from nnfs.datasets import spiral_data
 
 nnfs.init()
 
@@ -49,6 +50,7 @@ X =  [[1, 2, 3, 2.5],
       [2.0, 5.0, -1.0, 2.0],
       [-1.5, 2.7, 3.3, -0.8]]
 
+X, y = spiral_data(100, 3)
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -66,12 +68,15 @@ class Activation_ReLU:
         self.output = np.maximum(0, inputs)
 
 
-layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
+layer1 = Layer_Dense(2,5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
+
 # print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+
+activation1.forward(layer1.output)
+print(activation1.output)
+
 
 
