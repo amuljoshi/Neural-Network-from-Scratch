d722918e2b193573653ce5d6785bcb5647e1ed21
amuljoshi <83940325+amuljoshi@users.noreply.github.com>
2023-03-07 17:05:20 +0545

    Commit pt 7

---
+++ Neural Network from Scratch Pt 7.py
@@ -0,0 +1,48 @@
+# Calculating Loss with Categorical Cross-Entropy
+
+'''
+
+solving for x
+
+e ** x = b
+
+'''
+
+
+'''
+
+
+import numpy as np
+import math
+
+b = 5.2
+
+print(np.log(b))
+
+print(math.e ** 1.6486586255873816)
+
+
+
+'''
+
+
+
+import math
+
+
+softmax_output = [0.7, 0.1, 0.2]
+target_output =  [1, 0, 0]
+
+loss = -(math.log(softmax_output[0])*target_output[0] +
+         math.log(softmax_output[1])*target_output[1] +
+         math.log(softmax_output[2])*target_output[2])
+
+print(loss)
+
+loss = -math.log(softmax_output[0])
+
+print(loss)
+
+
+print(-math.log(0.7))
+print(-math.log(0.5))
